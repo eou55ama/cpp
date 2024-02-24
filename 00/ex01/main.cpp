c04b6c4deff0120973ab48996e5c1160ce4c15f3
Erraouzi Oussama <eoussama@e1r5p9.1337.ma>
2024-02-24 07:56:30 +0100

    c_str

--- 00/ex01/main.cpp
+++ 00/ex01/main.cpp
@@ -6,24 +6,52 @@
 /*   By: eoussama <eoussama@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/10 22:12:38 by eoussama          #+#    #+#             */
-/*   Updated: 2024/02/21 20:32:09 by eoussama         ###   ########.fr       */
+/*   Updated: 2024/02/23 17:22:24 by eoussama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PhoneBook.hpp"
 #include <cstring>
-#include<unistd.h>  
+#include <cstdlib> 
+#include<unistd.h> 
+#include <stdlib.h>
+#include <ctype.h>
 
-int take_ind()
+int valid_ind(const char *str,int i)
 {
-    int i;
+    if (strlen(str) != 1 || !isdigit(str[0]))
+        return(std::cout << "invalid index \n",1);
+    return(0);
+}
+int take_ind(int i)
+{
+    std ::string num;
+    const char *str;
+    int b;
+    b = 1;
+    while(b)
+    {
     std::cout << "Enter an index : ";
-    std::cin >> i;
-    return(i);
+    std::getline(std::cin ,num);
+    str = num.c_str();
+    b = valid_ind(str,i);
+    }
+    return(atoi(str));
+}
+
+void set_empty(std :: string *args)
+{
+    int i = 0;
+    while(i < 5)
+    {
+        args[i] = "";
+        i++;
+    }
 }
 
 void take_args(std::string *args)
 {
+    set_empty(args);
         while (args[0].empty())
         {
             std::cout << "Enter first name : ";
@@ -56,16 +84,16 @@ int main ()
 {
     PhoneBook PhoneBook;
     std:: string args[5];
-    int i , x;
+    int i , x ;
     i = 0;
     std :: string buf;
     while ("EXIT" != buf)
     {
-        std::cout << i << "-> : ";
+        std::cout << "-> : ";
         std::getline (std::cin,buf);
         if ("SEARCH" == buf)
         {
-            x = take_ind();
+            x = take_ind(i);
             if ( x < i)
                 PhoneBook.print(x);
         }
